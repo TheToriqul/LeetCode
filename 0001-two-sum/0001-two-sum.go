e720b2231c90028d08819edb8e6d97ba471a9a23
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2023-09-23 00:07:32 +0800

    Time: 3 ms (94.58%), Space: 4.3 MB (14.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,10 +1,8 @@
 func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
     var ans []int
-    
     for idx, num := range nums {
         complement := target - num
-        
         if c, ok := m[complement]; ok {
             ans = []int{c, idx}
             break
